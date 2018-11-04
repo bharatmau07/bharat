6f2dd4b87c226f2067d606d1ba3cc54277b39cc7
bharat sharma <bharatmau07@gmail.com>
2018-11-03 21:37:18 -0700

    first init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+  $db = mysqli_connect('localhost','root','','walkholi_holiday');
+   
+   $sql = "select * from destinations";
+   $run = mysqli_query($sql);
+   print_r($run); die;
+
+
+?>
